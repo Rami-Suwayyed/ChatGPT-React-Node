9a92f597307a1b4260ed50a572792e2f144d0b61
Rami-Suwayyed <Rami.Suwayyed@Gmail.com>
2024-04-29 21:36:14 +0300

    openai

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ const App = () => {
 
         }
         try {
-            const response = await fetch('http://localhost:8000/completions', options)
+            const response = await fetch('https://chatgpt-react-node.onrender.com/completions', options)
             const data = await response.json()
             setMessage(data.choices[0].message)
         } catch (error) {
